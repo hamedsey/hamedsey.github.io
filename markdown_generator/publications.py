c09ea401d787a3149a05e6da9738045c1293ecaa
Hamed <hamed@gatech.edu>
2023-12-16 10:25:45 -0500

    modified pub gen script

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -98,8 +98,9 @@ for row, item in publications.iterrows():
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
         
-    md += "\nRecommended citation: " + item.citation
-    
+    #md += "\nRecommended citation: " + item.citation
+    md += item.citation
+
     md_filename = os.path.basename(md_filename)
        
     with open("../_publications/" + md_filename, 'w') as f:
